00b823f683d0fa0f11d8edf0f90a54c63609d48e
Adam Norris <anorris@rivasolutionsinc.com>
2021-11-24 19:35:09 -0500

    Added sonarqube kubernetes secret

--- kubernetes_secrets.tf
+++ kubernetes_secrets.tf
@@ -1,9 +1,9 @@
 resource "kubernetes_secret" "grafana" {
   metadata {
-    name        = "grafana-admin"
-    namespace   = "opencloudcx"
+    name      = "grafana-admin"
+    namespace = "opencloudcx"
     labels = {
-      "ConnectOutput"="true"
+      "ConnectOutput" = "true"
     }
   }
 
@@ -19,3 +19,26 @@ resource "kubernetes_secret" "grafana" {
     kubernetes_namespace.opencloudcx,
   ]
 }
+
+resource "kubernetes_secret" "sonarqube" {
+  metadata {
+    name      = "sonarqube-admin"
+    namespace = "jenkins"
+    labels = {
+      "ConnectOutput" = "true"
+    }
+  }
+
+  data = {
+    username = "admin"
+    password = random_password.sonarqube.result
+  }
+
+  type = "kubernetes.io/basic-auth"
+
+  depends_on = [
+    module.eks,
+    kubernetes_namespace.jenkins,
+  ]
+}
+
