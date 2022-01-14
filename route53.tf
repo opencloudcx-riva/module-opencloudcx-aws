76bfdbaabd1a22fe1efbf90ab23bd00f76265051
Adam Norris <anorris@rivasolutionsinc.com>
2022-01-14 13:59:52 -0500

    Added DNS records for certificate and sonarqube-insecure

--- route53.tf
+++ route53.tf
@@ -18,6 +18,18 @@ resource "aws_route53_record" "ns" {
   records = aws_route53_zone.sub_domain.name_servers
 }
 
+resource "aws_route53_record" "mgmt-cert" {
+  zone_id = aws_route53_zone.sub_domain.zone_id
+  name    = var.aws_certificate_cname
+  type    = "CNAME"
+  ttl     = "300"
+  records = [var.aws_certificate_cname_value]
+
+  depends_on = [
+    aws_route53_zone.sub_domain
+  ]
+}
+
 resource "aws_route53_record" "jenkins_cname" {
   zone_id = aws_route53_zone.sub_domain.zone_id
   name    = "jenkins.${local.full_dns_zone}"
@@ -41,6 +53,19 @@ resource "aws_route53_record" "jenkins_insecure_cname" {
   depends_on = [
     helm_release.ingress-controller,
     helm_release.jenkins,
+  ]
+}
+
+resource "aws_route53_record" "sonarqube_insecure_cname" {
+  zone_id = aws_route53_zone.sub_domain.zone_id
+  name    = "sonarqube-insecure.${local.full_dns_zone}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [data.kubernetes_service.ingress_nginx.status.0.load_balancer.0.ingress.0.hostname]
+
+  depends_on = [
+    helm_release.ingress-controller,
+    helm_release.sonarqube,
   ]
 }
 
@@ -83,18 +108,18 @@ resource "aws_route53_record" "grafana_cname" {
   ]
 }
 
-resource "aws_route53_record" "selenium_cname" {
-  zone_id = aws_route53_zone.sub_domain.zone_id
-  name    = "selenium.${local.full_dns_zone}"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [data.kubernetes_service.ingress_nginx.status.0.load_balancer.0.ingress.0.hostname]
+# resource "aws_route53_record" "selenium_cname" {
+#   zone_id = aws_route53_zone.sub_domain.zone_id
+#   name    = "selenium.${local.full_dns_zone}"
+#   type    = "CNAME"
+#   ttl     = "300"
+#   records = [data.kubernetes_service.ingress_nginx.status.0.load_balancer.0.ingress.0.hostname]
 
-  depends_on = [
-    helm_release.ingress-controller,
-    helm_release.selenium3_grid
-  ]
-}
+#   depends_on = [
+#     helm_release.ingress-controller,
+#     helm_release.selenium3_grid
+#   ]
+# }
 
 resource "aws_route53_record" "spinnaker_gate_cname" {
   zone_id = aws_route53_zone.sub_domain.zone_id
