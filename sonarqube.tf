de75eea13b55a9d541d9343f11a6e7dbdea85ee2
Adam Norris <anorris@rivasolutionsinc.com>
2022-02-28 22:19:17 -0500

    Removed sonarqube config definition

--- sonarqube.tf
+++ sonarqube.tf
@@ -1,19 +1,19 @@
-terraform {
-  required_providers {
-    sonarqube = {
-      source = "jdamata/sonarqube"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     sonarqube = {
+#       source = "jdamata/sonarqube"
+#     }
+#   }
+# }
 
-provider "sonarqube" {
-  user                     = "admin"
-  pass                     = random_password.sonarqube.result
-  host                     = "https://sonarqube.${local.full_dns_zone}"
-  tls_insecure_skip_verify = true
-}
+# provider "sonarqube" {
+#   user                     = "admin"
+#   pass                     = random_password.sonarqube.result
+#   host                     = "https://sonarqube.${local.full_dns_zone}"
+#   tls_insecure_skip_verify = true
+# }
 
-resource "sonarqube_webhook" "webhook" {
-  name = "jenkins-webhook"
-  url  = "http://jenkins.jenkins.svc.cluster.local:8080/sonarqube-webhook/"
-}
+# resource "sonarqube_webhook" "webhook" {
+#   name = "jenkins-webhook"
+#   url  = "http://jenkins.jenkins.svc.cluster.local:8080/sonarqube-webhook/"
+# }
