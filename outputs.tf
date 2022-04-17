b11afcf2bf9c68b7ccd34aecbd5cf93019c6133b
Adam Norris <anorris@rivasolutionsinc.com>
2022-04-17 19:43:37 -0400

    Updated outputs to show URLs of services created

--- outputs.tf
+++ outputs.tf
@@ -1,38 +1,19 @@
-# Developed by RIVA Solutions Inc 2022.  Authorized Use Only
-
-output "aws_eks_cluster_endpoint" {
-  value = data.aws_eks_cluster.cluster.endpoint
-}
-
-output "aws_eks_cluster_auth_token" {
-  value = data.aws_eks_cluster_auth.cluster.token
-}
-
-output "aws_eks_cluster_ca_certificate" {
-  value = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-}
-
-output "aws_eks_cluster" {
-  value = data.aws_eks_cluster.cluster
-}
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "grafana_secret" {
-  value = random_password.grafana.result
-}
-
-output "jenkins_secret" {
-  value = random_password.jenkins.result
-}
-
 output "jenkins_url" {
-  value = "jenkins-insecure.${local.full_dns_zone}"
+  value = aws_route53_record.jenkins_cname.name
+}
+
+output "sonarqube_url" {
+  value = aws_route53_record.sonarqube_cname.name
+}
+
+output "dashboard_url" {
+  value = aws_route53_record.k8s_dashboard_cname.name
+}
+
+output "spinnaker_url" {
+  value = aws_route53_record.spinnaker_cname.name
 }
 
 output "spinnaker_gate_url" {
-  value = "spinnaker-gate.${local.full_dns_zone}"
+  value = aws_route53_record.spinnaker_gate_cname.name
 }
-
