aed05b148d47a385d03b42c05e96bbf4042a3779
Adam Norris <anorris@rivasolutionsinc.com>
2022-04-17 19:46:35 -0400

    Updated file with placeholders

--- main.tf
+++ main.tf
@@ -1,68 +1,38 @@
-# Developed by RIVA Solutions Inc 2022.  Authorized Use Only
+# # Developed by RIVA Solutions Inc 2022.  Authorized Use Only
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    token                  = data.aws_eks_cluster_auth.cluster.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+terraform {
+  required_providers {
+    kubernetes = {}
+    helm       = {}
   }
 }
 
-data "aws_partition" "current" {}
+# terraform {
+#   required_providers {
+#     aws = "~> 3.74"
+#   }
+# }
 
-data "aws_region" "current" {}
+# provider "aws" {
+#   region     = var.aws_region
+#   access_key = var.access_key
+#   secret_key = var.secret_key
+# }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
+# provider "kubernetes" {
+#   host                   = var.kubernetes_cluster_endpoint
+#   token                  = var.kubernetes_auth_token
+#   cluster_ca_certificate = var.kubernetes_cluster_ca_certificate
+# }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = var.kubernetes_cluster_endpoint
+#     token                  = var.kubernetes_auth_token
+#     cluster_ca_certificate = var.kubernetes_cluster_ca_certificate
+#   }
+# }
 
-data "aws_availability_zones" "available" {
-}
-
-# tags
 locals {
-  suffix             = random_string.scope.result
-  artifact-repo-name = join("-", compact(["artifact", var.stack, local.suffix]))
-  region             = var.region
-  name               = format("%s-%s", var.name, var.stack)
-  cluster_version    = var.cluster_version
-
-  name-tag               = { "Name" = local.name }
-  db-name-tag            = { "Name" = join("-", compact([local.name, "db"])) }
-  vpc-name-tag           = { "Name" = join("-", compact([local.name, "vpc"])) }
-  igw-name-tag           = { "Name" = join("-", compact([local.name, "igw"])) }
-  ngw-name-tag           = { "Name" = join("-", compact([local.name, "ngw"])) }
-  public-route-name-tag  = { "Name" = join("-", compact([local.name, "public-route"])) }
-  private-route-name-tag = { "Name" = join("-", compact([local.name, "private-route"])) }
-  private-dns-name-tag   = { "Name" = join("-", compact([local.name, "private-dns"])) }
-
   full_dns_zone = format("%s.%s", var.stack, var.dns_zone)
-
-  # kubernetes tags
-  vpc-k8s-shared-tag = {
-    format("kubernetes.io/cluster/%s", local.name) = "shared"
-  }
-  vpc-k8s-owned-tag = {
-    "key"                 = format("kubernetes.io/cluster/%s", local.name)
-    "value"               = "owned"
-    "propagate_at_launch" = "true"
-  }
 }
-
-resource "random_string" "scope" {
-  length  = 8
-  upper   = false
-  number  = false
-  special = false
-}
-
