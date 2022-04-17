31924c26f936ab76b06d30f1a817db9fdd1bda4c
Adam Norris <anorris@rivasolutionsinc.com>
2022-04-17 19:44:40 -0400

    Update dns entries

--- route53.tf
+++ route53.tf
@@ -1,39 +1,11 @@
 # Developed by RIVA Solutions Inc 2022.  Authorized Use Only
 
-data "aws_route53_zone" "vpc" {
-  name = var.dns_zone
-}
-
-resource "aws_route53_zone" "sub_domain" {
+data "aws_route53_zone" "zone" {
   name = local.full_dns_zone
-
-  tags = {
-    stack = var.stack
-  }
 }
-
-resource "aws_route53_record" "ns" {
-  zone_id = data.aws_route53_zone.vpc.zone_id
-  name    = local.full_dns_zone
-  type    = "NS"
-  ttl     = "30"
-  records = aws_route53_zone.sub_domain.name_servers
-}
-
-# resource "aws_route53_record" "mgmt-cert" {
-#   zone_id = aws_route53_zone.sub_domain.zone_id
-#   name    = var.aws_certificate_cname
-#   type    = "CNAME"
-#   ttl     = "300"
-#   records = [var.aws_certificate_cname_value]
-
-#   depends_on = [
-#     aws_route53_zone.sub_domain
-#   ]
-# }
 
 resource "aws_route53_record" "jenkins_cname" {
-  zone_id = aws_route53_zone.sub_domain.zone_id
+  zone_id = data.aws_route53_zone.zone.zone_id
   name    = "jenkins.${local.full_dns_zone}"
   type    = "CNAME"
   ttl     = "300"
@@ -46,7 +18,7 @@ resource "aws_route53_record" "jenkins_cname" {
 }
 
 resource "aws_route53_record" "jenkins_insecure_cname" {
-  zone_id = aws_route53_zone.sub_domain.zone_id
+  zone_id = data.aws_route53_zone.zone.zone_id
   name    = "jenkins-insecure.${local.full_dns_zone}"
   type    = "CNAME"
   ttl     = "300"
@@ -58,21 +30,8 @@ resource "aws_route53_record" "jenkins_insecure_cname" {
   ]
 }
 
-# resource "aws_route53_record" "sonarqube_insecure_cname" {
-#   zone_id = aws_route53_zone.sub_domain.zone_id
-#   name    = "sonarqube-insecure.${local.full_dns_zone}"
-#   type    = "CNAME"
-#   ttl     = "300"
-#   records = [data.kubernetes_service.ingress_nginx.status.0.load_balancer.0.ingress.0.hostname]
-
-#   depends_on = [
-#     helm_release.ingress-controller,
-#     helm_release.sonarqube,
-#   ]
-# }
-
 resource "aws_route53_record" "k8s_dashboard_cname" {
-  zone_id = aws_route53_zone.sub_domain.zone_id
+  zone_id = data.aws_route53_zone.zone.zone_id
   name    = "dashboard.${local.full_dns_zone}"
   type    = "CNAME"
   ttl     = "300"
@@ -85,7 +44,7 @@ resource "aws_route53_record" "k8s_dashboard_cname" {
 }
 
 resource "aws_route53_record" "keycloak_cname" {
-  zone_id = aws_route53_zone.sub_domain.zone_id
+  zone_id = data.aws_route53_zone.zone.zone_id
   name    = "keycloak.${local.full_dns_zone}"
   type    = "CNAME"
   ttl     = "300"
@@ -98,7 +57,7 @@ resource "aws_route53_record" "keycloak_cname" {
 }
 
 resource "aws_route53_record" "grafana_cname" {
-  zone_id = aws_route53_zone.sub_domain.zone_id
+  zone_id = data.aws_route53_zone.zone.zone_id
   name    = "grafana.${local.full_dns_zone}"
   type    = "CNAME"
   ttl     = "300"
@@ -110,21 +69,8 @@ resource "aws_route53_record" "grafana_cname" {
   ]
 }
 
-# resource "aws_route53_record" "selenium_cname" {
-#   zone_id = aws_route53_zone.sub_domain.zone_id
-#   name    = "selenium.${local.full_dns_zone}"
-#   type    = "CNAME"
-#   ttl     = "300"
-#   records = [data.kubernetes_service.ingress_nginx.status.0.load_balancer.0.ingress.0.hostname]
-
-#   depends_on = [
-#     helm_release.ingress-controller,
-#     helm_release.selenium3_grid
-#   ]
-# }
-
 resource "aws_route53_record" "spinnaker_gate_cname" {
-  zone_id = aws_route53_zone.sub_domain.zone_id
+  zone_id = data.aws_route53_zone.zone.zone_id
   name    = "spinnaker-gate.${local.full_dns_zone}"
   type    = "CNAME"
   ttl     = "300"
@@ -137,7 +83,7 @@ resource "aws_route53_record" "spinnaker_gate_cname" {
 }
 
 resource "aws_route53_record" "spinnaker_cname" {
-  zone_id = aws_route53_zone.sub_domain.zone_id
+  zone_id = data.aws_route53_zone.zone.zone_id
   name    = "spinnaker.${local.full_dns_zone}"
   type    = "CNAME"
   ttl     = "300"
@@ -150,7 +96,7 @@ resource "aws_route53_record" "spinnaker_cname" {
 }
 
 resource "aws_route53_record" "sonarqube_cname" {
-  zone_id = aws_route53_zone.sub_domain.zone_id
+  zone_id = data.aws_route53_zone.zone.zone_id
   name    = "sonarqube.${local.full_dns_zone}"
   type    = "CNAME"
   ttl     = "300"
